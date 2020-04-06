76188d31ca467f64cf972f8269b7403d4bdcdea8
Tim Duffey <decoy31@hotmail.com>
2020-04-05 22:23:42 -0700

    Created more sections for the COVID-19 page.

---
+++ src/pages/covid-19/components/WhoWeAreSection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+// Components
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+// Styles
+import WhoWeAreSectionStyles from './WhoWeAreSection.module.css';
+
+export default function OurNeedsSection() {
+	return (
+		<section className={WhoWeAreSectionStyles.root}>
+			<Container>
+				<Grid
+					container
+					justify="center"
+					alignItems="flex-start"
+					spacing={5}>
+					<Grid
+						className={WhoWeAreSectionStyles.content}
+						item
+						xs={12}>
+						<Typography component="h2" variant="h3">
+							Who We Are
+						</Typography>
+						<Typography component="p">
+							Joey Sim is a mechanical engineer based out of
+							Keyport, WA. It was his idea to start printing these
+							masks for medical staff and first responders.
+							Adrienne Landaal is an RN at St. Anthony Hospital in
+							Gig Harbor, WA. She is on the front lines helping
+							patients directly impacted by Coronavirus and
+							consulting on the project. If you're in Kitsap
+							County and want more resources and updates, check
+							out{' '}
+							<Link
+								to="https://kitsappublichealth.org/"
+								target="_blank"
+								rel="noopener">
+								Kitsap Public Health
+							</Link>
+							.
+						</Typography>
+					</Grid>
+				</Grid>
+			</Container>
+		</section>
+	);
+}
--- src/pages/covid-19/index.js
+++ src/pages/covid-19/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 // Components
 import Helmet from 'react-helmet';
@@ -7,13 +7,16 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import NeedPPECard from './components/NeedPPECard';
 import DonateCard from './components/DonateCard';
+import OurGoalSection from './components/OurGoalSection';
+import OurNeedsSection from './components/OurNeedsSection';
+import WhoWeAreSection from './components/WhoWeAreSection';
 
 // Styles
 import Covid19PageStyles from './index.module.css';
 
 export default function Covid19Page() {
 	return (
-		<div className={Covid19PageStyles.hero}>
+		<Fragment>
 			<Helmet>
 				<title>Kitsap Makers - COVID-19 Effort</title>
 				<meta
@@ -22,32 +25,37 @@ export default function Covid19Page() {
 							printing various PPE to support the fight against COVID-19."
 				/>
 			</Helmet>
-			<Container>
-				<Grid
-					container
-					justify="center"
-					alignItems="flex-start"
-					spacing={5}>
+			<section className={Covid19PageStyles.hero}>
+				<Container>
 					<Grid
-						item
-						className={Covid19PageStyles.heroContent}
-						xs={12}>
-						<Typography component="h1" variant="h2">
-							COVID-19 Effort
-						</Typography>
-						<Typography component="p" variant="subtitle1">
-							A group of volunteers from Kitsap Makers is 3D
-							printing various PPE.
-						</Typography>
+						container
+						justify="center"
+						alignItems="flex-start"
+						spacing={5}>
+						<Grid
+							item
+							className={Covid19PageStyles.heroContent}
+							xs={12}>
+							<Typography component="h1" variant="h2">
+								COVID-19 Effort
+							</Typography>
+							<Typography component="p" variant="subtitle1">
+								A group of volunteers from Kitsap Makers is 3D
+								printing various PPE.
+							</Typography>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<NeedPPECard />
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<DonateCard />
+						</Grid>
 					</Grid>
-					<Grid item xs={12} sm={6}>
-						<NeedPPECard />
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<DonateCard />
-					</Grid>
-				</Grid>
-			</Container>
-		</div>
+				</Container>
+			</section>
+			<OurGoalSection />
+			<OurNeedsSection />
+			<WhoWeAreSection />
+		</Fragment>
 	);
 }
